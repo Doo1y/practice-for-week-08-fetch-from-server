b7661ec9cca1ed1a3441cb57796300d560ce9038
Samuel Park <s.park111497@gmail.com>
2022-10-03 10:30:03 -0700

    finished practice

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,93 @@
 export function getAllDogs() {
-    // Your code here
-}
+  const url = '/dogs';
+  const headers = { 'Content-Type': 'html/text' };
+  let body;
+  
+  const options = {
+    method: 'GET',
+    headers: headers,
+    body: body
+  };
+
+  return fetch(url, options);
+};
 
 export function getDogNumberTwo() {
-    // Your code here
-}
+    const url = '/dogs/2'
+    const headers = {'Content-Type': 'html/text'};
+    let body;
+
+    const options = {
+      method: 'GET',
+      headers: headers,
+      body: body
+    };
+
+    return fetch(url, options);
+};
 
 export function postNewDog() {
-    // Your code here
-}
+  const url = '/dogs';
+  const headers = { 'Location': `application/x-www-form-urlencoded` };
+  let body = new URLSearchParams({
+    name: 'opal',
+    age: 4
+  });
+
+  const options = {
+    method: 'POST',
+    headers: headers,
+    body: body
+  }
+
+  return fetch(url, options)
+};
 
 export function postNewDogV2(name, age) {
-     // Your code here
-}
+  const url = '/dogs';
+  const headers = { 'Location': `application/x-www-form-urlencoded` };
+  let body = new URLSearchParams({
+    name: name,
+    age: age
+  });
+
+  const options = {
+    method: 'POST',
+    headers: headers,
+    body: body
+  }
+
+  return fetch(url, options)
+};
 
 export function deleteDog(id) {
-      // Your code here
+  const url = `/dogs/${id}/delete`;
+  const headers = { 'AUTH': 'ckyut5wau0000jyv5bsrud90y' };
+  let body = new URLSearchParams({
+    name: id.name,
+    age: id.age
+  })
+
+  const options = {
+    method: 'POST',
+    headers: headers,
+    body: body
+  }
+
+  return fetch(url, options);
 }
+
+// const url = "/endpoint";
+// const headers = { "Content-Type": "Request body's Content-Type" };
+// // Use the URLSearchParams API to format your body as shown below
+// const body = new URLSearchParams({
+//   key: "value"
+// });
+
+// const options = {
+//   method: "GET|POST|PUT|DELETE",
+//   headers: headers,
+//   body: body
+// };
+
+// fetch(url, options);
